5877f013321c4d0c2e34c9ece7cfbc872351c8b9
Ankit Sharma <ankit10594@gmail.com>
2022-08-10 14:06:36 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Home';
+import ProductDetails from './src/ProductDetails';
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ProductDetails" component={ProductDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
